3fa150409c726a5830e1f57d9d3ac0c192c1f75d
wazeveve <gabrielveanancio1123@gmail.com>
2026-02-02 16:01:18 -0300

    feat: Battle Game Challenge, Hero vs Monster using while

--- Part3/DoWhile.cs
+++ Part3/DoWhile.cs
@@ -21,3 +21,26 @@ while (current >= 3)
 Console.WriteLine($"Last number: {current}");
 
 */
+
+// Chaalenge - Battle Game
+
+int heroHealth = 10;
+int monsterHealth = 10;
+int attack = random.Next(1, 11);
+
+while (heroHealth > 0 && monsterHealth > 0)
+{
+    attack = random.Next(1, 11);
+    monsterHealth -= attack;
+    Console.WriteLine($"Hero attacks!\nMonster has lost {attack} HP;\nMonster's HP: {monsterHealth}");
+    if (monsterHealth <= 0) break;
+    attack = random.Next(1, 11);
+    heroHealth -= attack;
+    Console.WriteLine($"Monster attacks!\nHero has lost {attack} HP;\nHero's HP: {heroHealth}");
+    if (heroHealth <= 0) break;
+}
+
+if (heroHealth <= 0)
+    Console.WriteLine("Monster Wins!!");
+else
+    Console.WriteLine("Hero Wins!!");
