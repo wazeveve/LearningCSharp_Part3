8830293d131a969bbe6bf6fd389402a931a88f56
wazeveve <gabrielveanancio1123@gmail.com>
2026-02-02 15:47:48 -0300

    refactor: using continue in a do while statement

--- Part3/DoWhile.cs
+++ Part3/DoWhile.cs
@@ -1,18 +1,23 @@
 ﻿Random random = new Random();
 int current = random.Next(1, 11);
 
-/*
+
 do
 {
-    current = random.Next(1, 84151818);
+    current = random.Next(1, 135);
+
+    if (current >= 8) continue;
+
     Console.WriteLine(current);
 } while (current != 7);
 
-*/
 
+/*
 while (current >= 3)
 {
     Console.WriteLine(current);
     current = random.Next(1, 369);
 }
 Console.WriteLine($"Last number: {current}");
+
+*/
