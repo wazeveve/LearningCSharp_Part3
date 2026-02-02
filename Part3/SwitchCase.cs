86b7c52b763912a8c5c82db6d2fa86de65d28644
wazeveve <gabrielveanancio1123@gmail.com>
2026-02-02 09:18:47 -0300

    feat: switching if-elseif-else with switch statements

--- Part3/SwitchCase.cs
+++ Part3/SwitchCase.cs
@@ -35,51 +35,49 @@ string type = "";
 string color = "";
 string size = "";
 
-if (product[0] == "01")
+switch(product[0])
 {
-    type = "Sweat shirt";
-}
-else if (product[0] == "02")
-{
-    type = "T-Shirt";
-}
-else if (product[0] == "03")
-{
-    type = "Sweat pants";
-}
-else
-{
-    type = "Other";
-}
-
-if (product[1] == "BL")
-{
-    color = "Black";
-}
-else if (product[1] == "MN")
-{
-    color = "Maroon";
-}
-else
-{
-    color = "White";
+    case "01":
+        type = "Sweat Shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Sweat Pants";
+        break;
+    default:
+        type = "Other";
+        break;
 }
 
-if (product[2] == "S")
+switch(product[1])
 {
-    size = "Small";
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
 }
-else if (product[2] == "M")
+
+switch(product[2])
 {
-    size = "Medium";
-}
-else if (product[2] == "L")
-{
-    size = "Large";
-}
-else
-{
-    size = "One Size Fits All";
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
 }
 
 Console.WriteLine($"Product: {size} {color} {type}");
