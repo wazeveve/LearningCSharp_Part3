2ae9a147fce1b8973559728f9ced299965d4aba6
wazeveve <gabrielveanancio1123@gmail.com>
2026-02-02 15:45:32 -0300

    feat: Using while statement

--- Part3/DoWhile.cs
+++ Part3/DoWhile.cs
@@ -1,8 +1,18 @@
 ﻿Random random = new Random();
-int current = 0;
+int current = random.Next(1, 11);
 
+/*
 do
 {
     current = random.Next(1, 84151818);
     Console.WriteLine(current);
 } while (current != 7);
+
+*/
+
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = random.Next(1, 369);
+}
+Console.WriteLine($"Last number: {current}");
