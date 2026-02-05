28baa05c0818d66379e780326e99fcf8af75af5c
wazeveve <gabrielveanancio1123@gmail.com>
2026-02-05 09:47:06 -0300

    feat: testing a loop to add new pet data

--- Part3/GuidedProject/Program.cs
+++ Part3/GuidedProject/Program.cs
@@ -126,9 +126,45 @@ do
             readResult = Console.ReadLine();
             break;
         case "2":
-            Console.WriteLine("this app feature is coming soon - please check back to see progress.");
-            Console.WriteLine("Press enter key to continue.");
-            readResult = Console.ReadLine();
+            string anotherPet = "y";
+            int petCount = 0;
+            for (int i = 0; i < maxPets; i++)
+            {
+                if (ourAnimals[i, 0] != "ID #: ")
+                {
+                    petCount += 1;
+                }
+            }
+
+            if(petCount < maxPets)
+            {
+                Console.WriteLine($"We currently have {petCount} pets that need homes. We can manage {(maxPets - petCount)} more");
+            }
+
+            while (anotherPet == "y" && petCount < maxPets)
+            {
+                petCount = petCount + 1;
+                if (petCount < maxPets)
+                {
+                    Console.WriteLine("Do you want to enter info for another pet? (y/n))");
+                    do
+                    {
+                        readResult = Console.ReadLine();
+                        if (readResult != null)
+                        {
+                            anotherPet = readResult.ToLower();
+                        }
+                    } while (anotherPet != "y" && anotherPet != "n");
+                }
+            }
+
+            if (petCount >= maxPets)
+            {
+                Console.WriteLine("We have reached our limit on the number of pets that we can manage");
+                Console.WriteLine("Press the Enter key to continue.");
+                readResult = Console.ReadLine();
+            }
+ 
             break;
         case "3":
             Console.WriteLine("Challenge Project - please check back soon to see progress.");
